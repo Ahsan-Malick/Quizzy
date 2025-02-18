48ac0f2156a7d4047a23b33e252b326b38d6da7b
Ahsan Malik <ahsanjaved74@yahoo.com>
2025-02-18 20:58:23 +0000

    removed unwanted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
 import Home from './components/Home'
-import { useStore } from "./store/store"
+
 
 function App() {
   
- const getQuestionsAsync = useStore((state)=>state.getQuestionsAsync)
- const check = useStore((state)=>state.checkQuestionAttemptAsync)
- const validateAuthAsync = useStore((state)=>state.validateAuthAsync)
-
- const [isloggedin, setIsLogedIn] = useState<boolean>(false)
- 
-
- useEffect(()=>{
-  const fetchQuestions = async () => {
-    try {
-      await getQuestionsAsync();
-    } catch (error) {
-      console.error("Error fetching questions:", error);
-    }
-  };
-  const validateAuth = async () => {
-    await validateAuthAsync();
-  };
-  validateAuth();
-
-  fetchQuestions();
- },[getQuestionsAsync, validateAuthAsync])
-
 
   return (
     <>
-    <Home userLogged={isloggedin}></Home>
-   
+    <Home ></Home>
    </>
   )
 }
--- src/pages/resultpage.tsx
+++ src/pages/resultpage.tsx
@@ -1,22 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import QuizResult from "../components/Result"
 import { useStore } from '../store/store'
+import { User } from '../store/store'
 
 
 
-type Question = {
-    id: string;
-    question: string;
-    options: string[];
-    answer: string;
-  };
-  type User = {
-    firstname: string;
-    lastname: string;
-    username: string;
-    email: string;
-    disabled: boolean;
-  };
 
 const ResultPage:React.FC = () => {
 
